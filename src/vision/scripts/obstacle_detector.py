42d9a76ab32454fb5c3be1e297469a59f587fa5e
BFFV <bffarias@uc.cl>
2022-04-07 21:07:31 -0400

    :sparkles: Finished simple obstacle detector.

--- src/vision/scripts/obstacle_detector.py
+++ src/vision/scripts/obstacle_detector.py
@@ -2,7 +2,6 @@
 
 import cv2
 import numpy as np
-import roslib
 import rospy
 from cv_bridge import CvBridge
 from sensor_msgs.msg import Image
@@ -31,12 +30,31 @@ class TurtlebotKinect:
             self.detect_obstacles()
             rate.sleep()
 
+    # Check if objects in matrix partition are closer than 0.5 [m]
+    @staticmethod
+    def check_distance(matrix):
+        fixed_matrix = cv2.patchNaNs(matrix, 100)  # Replace NaN with big distance
+        depth_array = fixed_matrix.flatten()
+
+        # If any of the pixels have distance < 0.5 or NaN then there is an obstacle
+        return np.any(depth_array < 0.5) or np.any(depth_array >= 100)
+
     # Detect obstacles in estimated depth image
     def detect_obstacles(self):
+        # By default the robot assumes the path is free
         if self.depth is None:
             self.depth_pub.publish('free')
-        # TODO: detect obstacle in image matrix
-        self.depth_pub.publish(str(self.depth))
+            return
+
+        # Divide the depth image in 3 (left, center, right) and detect obstacles in each partition
+        detection = 'free'
+        if self.check_distance(self.depth[:, 212:426]):  # Center
+            detection = 'obstacle_center'
+        elif self.check_distance(self.depth[:, :212]):  # Left
+            detection = 'obstacle_left'
+        elif self.check_distance(self.depth[:, 426:]):  # Right
+            detection = 'obstacle_right'
+        self.depth_pub.publish(detection)  # Publish detection
 
 # Detect obstacles in Kinect images
 if __name__ == '__main__':

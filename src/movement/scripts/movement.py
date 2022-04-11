4030fa852b94e21bbd52c8359d78a576e32a07c5
Rafael Fernández <rafafdz01@gmail.com>
2022-04-11 15:11:23 -0400

    intermediate progress

--- src/movement/scripts/movement.py
+++ src/movement/scripts/movement.py
@@ -5,6 +5,49 @@ from geometry_msgs.msg import Twist, Pose
 from nav_msgs.msg import Odometry
 from tf.transformations import euler_from_quaternion
 
+from math import acos
+
+
+def vector_norm(x, y):
+    return (x ** 2 + y ** 2) ** 0.5
+
+def dot_product(x0, y0, x1, y1):
+    return x0 * x1 + y0 * y1
+
+def vector_angle(x0, y0, x1, y1):
+    denom = vector_norm(x0, y0) * vector_norm(x1, y1)
+    return acos(dot_product(x0, y0, x1, y1) / denom)
+     
+
+
+class RobotPose:
+    def __init__(self, x, y, theta):
+        self.x = x
+        self.y = y
+        self.theta = theta
+        
+    def linear_distance_to(self, other):
+        return vector_norm(other.x - self.x, other.y - self.y)
+        
+
+    def angular_distance_to(self, other):
+        return vector_angle(other.x, other.y, self.x, self.y)
+        
+        
+    def __add__(self, other):
+        self.x += other.x
+        self.y += other.y
+        self.theta += other.theta
+    
+    def __sub__(self, other):
+        self.x -= other.x
+        self.y -= other.y
+        self.theta -= other.theta
+        
+    def __repr__(self):
+        return f"<{self.x}, {self.y}, {self.theta}>"
+        
+
 class Movement:
     
     RATE_HZ = 5
@@ -17,6 +60,8 @@ class Movement:
         self.odom_sub = rospy.Subscriber( '/odom', Odometry, self.odometry_cb )
         self.real_pose_sub = rospy.Subscriber('/real_pose', Pose, self.real_pose_cb)
         self.rate = rospy.Rate(self.RATE_HZ)
+        self.initial_pose = RobotPose(1, 1, 0)
+        self.current_pose = self.initial_pose
     
     def apply_velocity(self, lin_vel_list, ang_vel_list, time_list):
         args = zip(lin_vel_list, ang_vel_list, time_list)
@@ -41,18 +86,62 @@ class Movement:
             self.cmd_vel_mux_pub.publish(speed)
         
     
-    def move_robot_to_destiny(self, goal_pose):
-        pass
+    def move_robot_to_destiny(self, goal_pose_list):
+        poses = [ RobotPose(*pose) for pose in goal_pose_list ] + [None]
+        rospy.loginfo(f"Current pos list {poses}")
+         
+        # TODO:s
+        # - Point to next point
+        # - Go forwards
+        # - Set orientation to target 
+        
+        
+        pose_iter = iter(poses)
+        current_pose = self.current_pose
+        next_pose = next(pose_iter)  
+        
+        velocities = []
+        
+        while next_pose:
+
+            # Align rotation
+            rospy.loginfo(f"Moving from {current_pose} to {next_pose}")
+            
+            diff_pose = next_pose - current_pose
+            fake_pos = RobotPose(1, 0, 0)
+            
+            
+            final_angle = fake_pos.angular_distance_to(diff_pose)
+            rospy.loginfo(f"The angle difference is {final_angle}")
+            
+            
+            # angle_diff = current_pose.angular_distance_to(next_pose)
+            # distance = current_pose.linear_distance_to(next_pose)
+            
+            # w_time = angle_diff / self.max_w 
+            # sign = 1 if angle_diff > 0 else -1
+            # velocities.append((0, sign * self.max_w, w_time))
+                    
+            # dist_time = distance / self.max_v
+            # velocities.append((self.max_v, 0, dist_time))
+                    
+            current_pose = next_pose
+            next_pose = next(pose_iter)
+            
+        # self.apply_velocity(*zip(*velocities))
+              
     
     def odometry_cb(self, odom):
         x = odom.pose.pose.position.x
         y = odom.pose.pose.position.y
-        z = odom.pose.pose.position.z
-        roll, pitch, yaw = euler_from_quaternion(( odom.pose.pose.orientation.x,
-                                                    odom.pose.pose.orientation.y,
-                                                    odom.pose.pose.orientation.z,
-                                                    odom.pose.pose.orientation.w ) )
-        rospy.loginfo(f'Current pose - lin: ({x}, {y}, {z}) ang: ({roll}, {pitch}, {yaw})')
+        orientation = odom.pose.pose.orientation
+        _, _, yaw = euler_from_quaternion(( orientation.x,
+                                            orientation.y,
+                                            orientation.z,
+                                            orientation.w))
+        
+        self.current_pose = self.initial_pose + RobotPose(x, y, yaw)
+        rospy.logdebug(f'Current pose - lin: ({x}, {y}) ang: ({yaw})')
     
     def real_pose_cb(self, odom):
         x = odom.position.x
@@ -62,12 +151,14 @@ class Movement:
                                                     odom.orientation.y,
                                                     odom.orientation.z,
                                                     odom.orientation.w ) )
-        rospy.loginfo(f'Real pose - lin: ({x}, {y}, {z}) ang: ({roll}, {pitch}, {yaw})')
+        rospy.logdebug(f'Real pose - lin: ({x}, {y}, {z}) ang: ({roll}, {pitch}, {yaw})')
 
 if __name__ == '__main__':
     mov = Movement()
-    mov.apply_velocity( 
-        [0.5], # 0, 2, 4],
-        [0], # 3.14159, 0, -0.2],   
-        [5.1], # 1, 4, 3] 
-    )
+    mov.move_robot_to_destiny([[0, 0, 1], [2, 1, 4]])
+    rospy.spin()  
+    # mov.apply_velocity( SS
+    #     [0.5], # 0, 2, 4],
+    #     [0], # 3.14159, 0, -0.2],   
+    #     [5.1], # 1, 4, 3] 
+    # )

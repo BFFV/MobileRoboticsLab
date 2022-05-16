6ea80d28e933e4b665e64a50a4c2ff2724d22564
BFFV <bffarias@uc.cl>
2022-05-15 23:05:53 -0400

    :sparkles: Single goal tracking.

--- src/low_level_control/scripts/pid_controller.py
+++ src/low_level_control/scripts/pid_controller.py
@@ -3,28 +3,32 @@
 import rospy
 from std_msgs.msg import Float64
 
+# PID Controller Wrapper
 class PIDController:
-    def __init__( self, topic:str = "controller"):
-        self.set_point = rospy.Publisher( f'/{topic}/setpoint', Float64, queue_size = 1 )
-        while self.set_point.get_num_connections() == 0 and not rospy.is_shutdown():
+    def __init__(self, topic='controller'):
+        # Reference value
+        self.set_point = rospy.Publisher(f'/{topic}/setpoint', Float64, queue_size=1)
+        while not rospy.is_shutdown() and self.set_point.get_num_connections() == 0:
             rospy.sleep(0.2)
 
-        self.state = rospy.Publisher( f'/{topic}/state', Float64, queue_size = 1 )
-        while self.state.get_num_connections() == 0 and not rospy.is_shutdown():
+        # Estimated real value
+        self.state = rospy.Publisher(f'/{topic}/state', Float64, queue_size=1)
+        while not rospy.is_shutdown() and self.state.get_num_connections() == 0:
             rospy.sleep(0.2)
 
+        # Actuation value
         rospy.Subscriber(f'/{topic}/control_effort', Float64, self.actuation)
-
         self.speed = 0
+        rospy.loginfo(f'Ready {topic} connections!')
 
-        rospy.loginfo( f"ready {topic} connections")
+    # Publish reference
+    def pub_set_point(self, set_point):
+        self.set_point.publish(set_point)
 
-    def pub_set_point( self, set_point ):
-        self.set_point.publish( set_point )
+    # Publish real value
+    def pub_state(self, state):
+        self.state.publish(state)
 
-    def pub_state( self, state ):
-        self.state.publish( state )
-
-    def actuation( self, data ):
-        self.speed = float( data.data )
-        # rospy.loginfo( 'speed received: %f' % ( self.speed ))
+    # Receive actuation value
+    def actuation(self, data):
+        self.speed = float(data.data)

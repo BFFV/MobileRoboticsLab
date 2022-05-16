842327541ff999a99f58d080a8c7d339996d485c
BFFV <bffarias@uc.cl>
2022-05-15 23:05:53 -0400

    :sparkles: Single goal tracking.

--- src/low_level_control/scripts/follow_the_carrot.py
+++ src/low_level_control/scripts/follow_the_carrot.py
@@ -1,14 +1,10 @@
 #!/usr/bin/env python3
 
-import cv2
 import numpy as np
 import rospy
-from cv_bridge import CvBridge, CvBridgeError
 from geometry_msgs.msg import Twist
 from nav_msgs.msg import Odometry
 from pid_controller import PIDController
-from sensor_msgs.msg import Image
-from std_msgs.msg import Float64
 from tf.transformations import euler_from_quaternion
 
 
@@ -18,46 +14,69 @@ class FollowTheCarrot:
         rospy.init_node('follow_the_carrot')
         self.variables_init()
         self.connections_init()
-        rospy.spin()
+        self.run()
 
+    # Initialize variables
     def variables_init(self):
-        # value and goal goal value
-        self.value = 0.0
-        self.goal_value = 141.0
+        # Orientation
+        self.angle = 0.0
+        self.goal_angle = 0.0
 
-        # last orientation and delta ang
-        self.last_value = 0.0
-        self.delta_value = 0.0
-        # message sending frequency: 10Hz
+        # Message frequency: 10Hz
         self.hz = 10
         self.rate_obj = rospy.Rate(self.hz)
-        # msg type: Twist
-        self.speed_msg = Twist()
-        # line speed
-        self.speed_msg.linear.x = 0.1
-        # controller topic, in launch "ns = topic"
-        self.topic = "robot_ang"
-        self.bridge = CvBridge()
 
+        # Speed message for actuation
+        self.speed_msg = Twist()
+        self.speed_msg.linear.x = 0.1
+
+        # Controller topic, namespace in launch file
+        self.topic = 'pathline_follower'
+
+    # Initialize connections
     def connections_init(self):
         # Turtlebot connections
         self.cmd_vel_mux_pub = rospy.Publisher('/yocs_cmd_vel_mux/input/navigation', Twist, queue_size=10)
 
-        #  angle PID controller
-        self.ang_PID_controller = PIDController(self.topic)
+        # Angle PID controller
+        self.angle_controller = PIDController(self.topic)
 
-        # set point
-        rospy.Subscriber('/goal_value', Float64, self.run)
+        # Odometry
+        rospy.Subscriber('/odom', Odometry, self.orientation)
 
-    def run(self, goal):
-        self.goal_value = goal.data
-        self.ang_PID_controller.pub_set_point(self.goal_value)
+    # Update orientation dynamically
+    def orientation(self, odom_data):
+        # Get odometry orientation info
+        pose_data = odom_data.pose.pose
+        quaternion = (pose_data.orientation.x,
+                      pose_data.orientation.y,
+                      pose_data.orientation.z,
+                      pose_data.orientation.w)
+        _, _, yaw = euler_from_quaternion(quaternion)
+        self.angle = yaw
+
+        # Map angles to [0,2*pi)
+        if self.angle < 0:
+            self.angle += 2 * np.pi
+
+        # Update controller with current state
+        self.angle_controller.pub_state(self.angle)
+
+    # Pathline tracking with low level control
+    def run(self):
         while True:
-            self.speed_msg.angular.z = self.ang_PID_controller.speed
+            # TODO: update goal
+            if self.goal_angle != np.pi:
+                self.goal_angle = np.pi
+                self.angle_controller.pub_set_point(self.goal_angle)
+
+            # Adjust orientation with controller
+            self.speed_msg.angular.z = self.angle_controller.speed
+
+            # Actuation
             self.cmd_vel_mux_pub.publish(self.speed_msg)
             self.rate_obj.sleep()
 
-# Follow the Carrot
+# Follow the Carrot pathline tracking
 if __name__ == '__main__':
-  follow_the_carrot = FollowTheCarrot()
-  rospy.spin()
+    follow_the_carrot = FollowTheCarrot()
